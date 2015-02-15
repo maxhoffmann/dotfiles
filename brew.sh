f3c866088e19d0bf78ceb0c29b5db6476a687d23
Maximilian Hoffmann <account@maximilianhoffmann.com>
2015-02-15 13:30:33 +0100

    update brew installation

--- brew.sh
+++ brew.sh
@@ -1,58 +1,57 @@
-# Install command-line tools using Homebrew
-# Usage: `brew bundle Brewfile`
+#!/usr/bin/env bash
 
 # Make sure we’re using the latest Homebrew
-update
+brew update
 
 # Upgrade any already-installed formulae
-upgrade
+brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated)
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-install coreutils
+brew install coreutils
 # Install some other useful utilities like `sponge`
-install moreutils
+brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-install findutils
+brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`
-install gnu-sed --default-names
+brew install gnu-sed --default-names
 # Install Bash 4
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
-install bash
-install bash-completion
+brew install bash
+brew brew install bash-completion
 
 # Install wget with IRI support
-install wget --enable-iri
+brew install wget --enable-iri
 
 # Install RingoJS and Narwhal
 # Note that the order in which these are installed is important; see http://git.io/brew-narwhal-ringo.
-install ringojs
-install narwhal
+brew install ringojs
+brew install narwhal
 
 # Install more recent versions of some OS X tools
-install vim --override-system-vi
-install homebrew/dupes/grep
-install homebrew/dupes/screen
-install homebrew/php/php55 --with-gmp
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/screen
+brew install homebrew/php/php55 --with-gmp
 
 # Install other useful binaries
 # install ack
 # install bfg
 #install exiv2
 # install foremost
-install git
+brew install git
 # install hashpump
 # install imagemagick --with-webp
 # install lynx
 # install nmap
-install node # This installs `npm` too using the recommended installation method
+# install node # This installs `npm` too using the recommended installation method
 # install p7zip
 # install pigz
 # install pv
 # install rename
 # install rhino
 # install sqlmap
-install tree
+brew install tree
 # install ucspi-tcp # `tcpserver` et al.
 # install webkit2png
 # install xpdf
@@ -61,4 +60,4 @@ install tree
 # install homebrew/versions/lua52
 
 # Remove outdated versions from the cellar
-cleanup
+brew cleanup
